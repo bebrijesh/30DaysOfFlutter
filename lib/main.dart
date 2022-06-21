e7989ba560d736c128892d0b0c1130e5ccd68963
Brijeshkumar Chavda <chavdabrijeshkumar25@gmail.com>
2022-06-21 10:57:33 +0530

    Day1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codepur/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: const Text("Flutter"),
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
