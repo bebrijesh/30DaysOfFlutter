3c642a032d53bebdb5e42bd6b546b01f2875af5b
Brijeshkumar Chavda <chavdabrijeshkumar25@gmail.com>
2022-06-22 13:56:39 +0530

    Day5 completed
    - SingleChildScrollView()f
    - minimumSize:
    - Size(w, h)
    - Navigator.pushNamed(context, MyRoutes.homeRoute);

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codepur/screens/home_screen.dart';
 import 'package:flutter_codepur/screens/login_screen.dart';
+import 'package:flutter_codepur/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp());
@@ -21,8 +22,8 @@ class MyApp extends StatelessWidget {
       initialRoute: "/LoginScreen",
       routes: {
         "/": (context) => LoginScreen(),
-        "/HomeScreen": (context) => HomeScreen(),
-        "/LoginScreen": (context) => LoginScreen(),
+        MyRoutes.homeRoute: (context) => HomeScreen(),
+        MyRoutes.loginRoute: (context) => LoginScreen(),
       },
     );
   }
